e9f1ba757e0c1402764e622add09d4eece7201d7
Francis Gurahoo <gurahoo.francis@heb.com>
2020-05-28 20:02:45 -0500

    added SAUCE as its own if block during runType webdriver setup
    set defaultSauceUrl in case a user-defined one isn't provided
    added tunnelIdentifier and parentTunnel to sauce:options capabilities object

--- framework/src/main/java/io/github/kgress/scaffold/webdriver/WebDriverManager.java
+++ framework/src/main/java/io/github/kgress/scaffold/webdriver/WebDriverManager.java
@@ -278,11 +278,12 @@ public class WebDriverManager {
             }
 
         // If the run type is sauce of grid, configure a remote browser
-        } else if (runType == SAUCE || runType == GRID) {
+        } else if (runType == GRID) {
             browserOptions.setCapability(SCREEN_RESOLUTION_CAPABILITY, screenResolution.getScreenShotResolutionAsString(SAUCELABS));
             webDriver = configureRemoteBrowser(browserOptions, testName);
-
         // If an unknown run type has been passed in, throw an error
+        } else if (runType == SAUCE) {
+            webDriver = configureRemoteBrowser(browserOptions, testName);
         } else {
             throw new WebDriverContextException(String
                     .format("Unknown run type: %s. Please check your configuration.", runType.getRunType()));
@@ -487,29 +488,30 @@ public class WebDriverManager {
         // In order to build the URi correctly, pull the username and access key from the desired capabilities bean.
         var username = sauce.getUserName();
         var accessKey = sauce.getAccessKey();
-        var sauceUrl = "ondemand.saucelabs.com/wd/hub";
+        var defaultSauceUrl = "@ondemand.saucelabs.com/wd/hub";
         var tunnelIdentifier = sauce.getTunnelIdentifier();
         var parentTunnel = sauce.getParentTunnel();
+        var sauceCaps = new MutableCapabilities();
+        var sauceConfigUrl = sauce.getUrl();
 
         try {
             if (sauce.getTunnelIdentifier() != null) {
-                browserOptions.setCapability("tunnelIdentifier", tunnelIdentifier);
+                sauceCaps.setCapability("tunnelIdentifier", tunnelIdentifier);
             }
             if (sauce.getParentTunnel() != null) {
-                browserOptions.setCapability("parentTunnel", parentTunnel);
+                sauceCaps.setCapability("parentTunnel", parentTunnel);
             }
-            if (remoteUrl == null) {
-                remoteUrl = URI.create("https://" + sauceUrl).toString();
+            if (sauceConfigUrl == null) {
+                sauceConfigUrl = URI.create("https://" + username + ":" + accessKey + defaultSauceUrl).toString();
             }
 
-            MutableCapabilities sauceCaps = new MutableCapabilities();
             sauceCaps.setCapability("username", username);
             sauceCaps.setCapability("accessKey", accessKey);
             sauceCaps.setCapability("name", testName);
             sauceCaps.setCapability("screenResolution", screenResolution.getScreenShotResolutionAsString(SAUCELABS));
-            browserOptions.setCapability("sauce:options", sauceCaps);
 
-            return startScreenshotRemoteDriver(remoteUrl, browserOptions);
+            browserOptions.setCapability("sauce:options", sauceCaps);
+            return startScreenshotRemoteDriver(sauceConfigUrl, browserOptions);
         } catch (Exception e) {
             throw new WebDriverContextException("Unable to start new remote session against saucelabs. Please check your " +
                     "configuration.", e);
@@ -537,4 +539,3 @@ public class WebDriverManager {
         }
     }
 }
-
